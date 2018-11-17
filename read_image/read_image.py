d273cb3f75d93a0df76a032ec1aac33950ae8744
unknown <1784489871@qq.com>
2018-11-17 13:52:40 +0800

    plt

--- read_image/read_image.py
+++ read_image/read_image.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def get_image_info(image):
 	print(type(image))
@@ -9,7 +9,8 @@ def get_image_info(image):
 	print(image.dtype)
 	print(np.array(image))
 
-
+img2 = cv.imread('Rick and Morty.jpg')
+img3 = cv.cvtColor(img2,cv.COLOR_BGR2RGB)
 img = cv.imread('Rick and Morty.jpg',0)
  
 cv.namedWindow("Rick and Morty", cv.WINDOW_NORMAL)
@@ -23,3 +24,10 @@ if k == ord("s"):
 if k == 27:
 	cv.destroyAllWindows()
 
+plt.subplot(311), plt.imshow(img,cmap= "gray")
+plt.title('GRAY Image'), plt.xticks([]),plt.yticks([])
+plt.subplot(312), plt.imshow(img2)
+plt.title('BGR Image'),plt.xticks([]),plt.yticks([])
+plt.subplot(313), plt.imshow(img3)
+plt.title('RGB Image'),plt.xticks([]),plt.yticks([])
+plt.show()
